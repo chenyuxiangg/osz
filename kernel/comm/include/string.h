76c4783bf0018f77779b2b33e1142a76120cb2af
chenyuxiangg <1007423165@qq.com>
2025-04-19 12:42:55 +0000

    [Discription]: add basic operation for string.
    [Feature or Bug]: Feature
    [Auther]: zyzs
    [For Future]:

--- kernel/comm/include/string.h
+++ kernel/comm/include/string.h
@@ -1,7 +1,10 @@
 #ifndef __STRING_H__
 #define __STRING_H__
 
-VOID *memset (VOID *dst, size_t value, size_t len);
-VOID *memcpy (VOID *dst, VOID * src, size_t len);
+VOID *memset(VOID *dst, size_t value, size_t len);
+VOID *memcpy(VOID *dst, VOID * src, size_t len);
+UINT32 strlen(VOID *str);
+UINT32 strncmp(VOID *str1, VOID *str2, UINT32 len);
+VOID *strncpy(VOID *dest, VOID *src, UINT32 len);
 
 #endif
