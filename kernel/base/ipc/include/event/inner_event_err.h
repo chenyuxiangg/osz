0e6014c402d091536ee48b5513e26978a51fe241
chenyuxiangg <1007423165@qq.com>
2025-10-19 03:48:34 +0000

    [Discription]: delete redundancy event file.
    [Feature or Bug]: Bug
    [Auther]: zyzs
    [For Future]:
    	1. add mutex and mutex ut.

--- kernel/base/ipc/include/event/inner_event_err.h
+++
@@ -1,9 +0,0 @@
-#ifndef __INNER_EVENT_ERR_H__
-#define __INNER_EVENT_ERR_H__
-
-#define EVENT_BASE_ERR                      (0x30)
-#define EVENT_OP_COMFLICT_ERR               (EVENT_BASE_ERR | 0x1)
-#define EVENT_OP_INVALID_ERR                (EVENT_BASE_ERR | 0x2)
-#define EVENT_WAIT_INVALID_ERR              (EVENT_BASE_ERR | 0x3)
-
-#endif
